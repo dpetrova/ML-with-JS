28e9dec44b3d7d933b6f5de15e07723317a58f8f
dpetrova <daniella_petrova@abv.bg>
2022-01-02 14:07:40 +0200

    some comments abot load-csv

--- 04_regressions/load-csv.js
+++ 04_regressions/load-csv.js
@@ -1,11 +1,14 @@
 const fs = require("fs");
 const _ = require("lodash");
-const shuffleSeed = require("shuffle-seed");
+const shuffleSeed = require("shuffle-seed"); //library to shuffle array of records
 
 //extract colums that we will use
 function extractColumns(data, columnNames) {
+  //get header row
   const headers = _.first(data);
+  //get indexes that want to extract
   const indexes = _.map(columnNames, (column) => headers.indexOf(column));
+  //exctract desired column data
   const extracted = _.map(data, (row) => _.pullAt(row, indexes));
   return extracted;
 }
@@ -20,24 +23,31 @@ module.exports = function loadCSV(
     splitTest = false, //split data into test/training sets?
   }
 ) {
-  //read csv data
+  //read csv data from a file
   let data = fs.readFileSync(filename, { encoding: "utf-8" });
-  data = _.map(data.split("\n"), (d) => d.split(","));
+  //split raw data into rows, and then each row into separate data
+  data = _.map(data.split("\n"), (row) => row.split(","));
+  //remove trailing empty columns if any
   data = _.dropRightWhile(data, (val) => _.isEqual(val, [""]));
   //get headers
   const headers = _.first(data);
 
   //convert or parse data except the header
   data = _.map(data, (row, index) => {
+    //skip header
     if (index === 0) {
       return row;
     }
+
+    //process data
     return _.map(row, (element, index) => {
+      //convert data if converter is provided (object with key=name_of_the_column, value=custom_function)
       if (converters[headers[index]]) {
         const converted = converters[headers[index]](element);
         return _.isNaN(converted) ? element : converted;
       }
 
+      //try to parse to number
       const result = parseFloat(element.replace('"', ""));
       return _.isNaN(result) ? element : result;
     });
@@ -46,21 +56,21 @@ module.exports = function loadCSV(
   //extract colums that we will use
   let labels = extractColumns(data, labelColumns);
   data = extractColumns(data, dataColumns);
-  //remove headers data
+  //remove first row, which is headers (column titles)
   data.shift();
   labels.shift();
 
   //shuffle data
   if (shuffle) {
-    data = shuffleSeed.shuffle(data, "phrase");
+    data = shuffleSeed.shuffle(data, "phrase"); //use a string as second argument to assure shuffle to be always in the same shuffling order when use identical seed phrase
     labels = shuffleSeed.shuffle(labels, "phrase");
   }
 
   //split data into test and training sets
   if (splitTest) {
     const trainSize = _.isNumber(splitTest)
-      ? splitTest
-      : Math.floor(data.length / 2);
+      ? splitTest //if splitTest is number
+      : Math.floor(data.length / 2); //if splitTest is boolean split data into half
 
     return {
       features: data.slice(trainSize),
